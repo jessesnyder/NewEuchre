b8872cd55b0075568c7871641c78b4ca514f4542
Colin Ong-Dean <congdean@gmail.com>
2020-11-28 00:18:14 -0800

    Test change

--- Euchre13.py
+++ Euchre13.py
@@ -1,5 +1,7 @@
 # This section is for setting global variables and importing methods.
 
+# Committed 11-28-2020
+
 card_values=[(x, y) for x in range(4) for y in range(6)]
 suitlabels=['Hearts','Spades','Diamonds','Clubs'] #Having same-color suits two apart enables actions recognizing their complementary relationship in Euchre.
 positionlabels=['9','10','Jack','Queen','King','Ace']
